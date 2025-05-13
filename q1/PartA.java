0a66a6b1a5c0b424fe07c9550d27c70871b20f91
nikolas.cabrera. <99770243+Menamechris@users.noreply.github.com>
2025-05-13 10:00:19 -0400

    Update PartA.java

--- q1/PartA.java
+++ q1/PartA.java
@@ -1,1 +1,14 @@
+public int walkDogs(int hour) {
+    //num of available dogs from company
+    int numDogs = numAvailableDogs(hour);
 
+    //if the maxDogs exceeds num of dogs available
+    
+    if(maxDogs>=numDogs) {
+        company.updateDogs(hour, numDogs);
+        return numDogs;
+    } else {
+        company.updateDogs(hour, maxDogs);
+        return maxDogs;
+    }
+}
